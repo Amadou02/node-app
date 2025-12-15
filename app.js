543539d9ef0ea9ef47e8ea7cade4990a1d61a7c9
Amadou <atidiane.camara@gmail.com>
2025-12-15 09:40:29 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+app = express();
+
+app.get("/api/", (req, res) => {
+  res.send("Hello, Node!");
+});
+
+app.listen(3000, () => {
+  console.log(`serveur is running at http://localhost:3000`);
+});
